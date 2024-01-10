4dfce4ce0a375575c99a5d1f37cbf8f212441b57
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2024-01-11 09:51:00 +1300

    user redirected to root path if individual blog not found

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+  get "/blog_posts/:id", to: "blog_posts#show"
+
+  
 
   # Defines the root path route ("/")
   root "blog_posts#index"
