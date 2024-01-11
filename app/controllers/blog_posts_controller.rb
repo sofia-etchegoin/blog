ef0d78d9e5923badb01bd69805a529c871d894e3
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2024-01-11 14:16:16 +1300

    refactored the controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,12 +1,10 @@
 class BlogPostsController < ApplicationController 
+  before_action :set_blog_post, except: [:index, :new, :create]
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound 
-    redirect_to root_path 
   end
 
   def new
@@ -23,11 +21,9 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -36,7 +32,6 @@ class BlogPostsController < ApplicationController
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
     redirect_to root_path
   end
@@ -47,4 +42,8 @@ class BlogPostsController < ApplicationController
     params.require(:blog_post).permit(:title, :body)
   end
 
+  def set_blog_post
+  @blog_post = BlogPost.find(params[:id])
+  end
+  
 end
